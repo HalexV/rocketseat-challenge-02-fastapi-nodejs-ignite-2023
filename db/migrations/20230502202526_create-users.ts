ecd223e1285b93feae579cef5715d4db4f44802d
Halex Viotto Gomes <halexx002@gmail.com>
2023-05-05 13:28:51 -0400

    fix: turn email unique

--- db/migrations/20230502202526_create-users.ts
+++ db/migrations/20230502202526_create-users.ts
@@ -3,7 +3,7 @@ import { type Knex } from 'knex';
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('users', (table) => {
     table.uuid('id').primary();
-    table.text('email').notNullable();
+    table.text('email').notNullable().unique();
     table.text('password').notNullable();
   });
 }
