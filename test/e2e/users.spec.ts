39530439554b1b95c42de92e36c9105543379286
Halex Viotto Gomes <halexx002@gmail.com>
2023-05-09 14:39:38 -0400

    test: should not authenticate user invalid data

--- test/e2e/users.spec.ts
+++ test/e2e/users.spec.ts
@@ -20,7 +20,7 @@ describe('Users routes', () => {
     await knex('users').del();
   });
 
-  describe('users/', () => {
+  describe('POST:users/', () => {
     it('should be able to create a new user', async () => {
       const userCreateResponse = await request(app.server).post('/users').send({
         email: 'test@test.com',
@@ -83,6 +83,41 @@ describe('Users routes', () => {
 
       expect(userCreateResponse.status).toBe(400);
       expect(userCreateResponse.body).toEqual(expectedResponseBody);
+    });
+  });
+
+  describe('POST:users/authenticate', () => {
+    it('should not be able to authenticate an user with invalid data', async () => {
+      const userAuthenticateResponse = await request(app.server)
+        .post('/users/authenticate')
+        .send({
+          email: 'test.test.com',
+          password: 123,
+        });
+
+      console.dir(userAuthenticateResponse.body, { depth: null });
+
+      const expectedResponseBody = {
+        issues: [
+          {
+            validation: 'email',
+            code: 'invalid_string',
+            message: 'Invalid email',
+            path: ['email'],
+          },
+          {
+            code: 'invalid_type',
+            expected: 'string',
+            received: 'number',
+            path: ['password'],
+            message: 'Expected string, received number',
+          },
+        ],
+        message: 'Validation issues!',
+      };
+
+      expect(userAuthenticateResponse.status).toBe(400);
+      expect(userAuthenticateResponse.body).toEqual(expectedResponseBody);
     });
   });
 });
