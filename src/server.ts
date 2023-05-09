c96a3be37f82d7ee83141198424119a1c428c9a4
Halex Viotto Gomes <halexx002@gmail.com>
2023-05-09 13:19:04 -0400

    feat: use environment port

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 /* eslint-disable @typescript-eslint/no-floating-promises */
 import { app } from './app';
+import { env } from './env';
 
 const start = async () => {
   try {
     await app.listen({
-      port: 3000,
+      port: env.PORT,
     });
-    console.log('Server running on port 3000');
+    console.log(`Server running on port ${env.PORT}`);
   } catch (error) {
     app.log.error(error);
   }
